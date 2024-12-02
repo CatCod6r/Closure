ed44982d9f99c66dfde6537f13b99deecbc89b04
CatCod6r <165474293+CatCod6r@users.noreply.github.com>
2024-12-02 14:08:49 +0000

    Update 2-array.js

--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const array = () => null;
+const createList = () => {
+  const elements = [];
+  const retrieve = (index) => elements[index];
+  retrieve.add = (item) => elements.push(item);
+  retrieve.remove = () => elements.pop();
+  return retrieve;
+};
 
 module.exports = { array };
